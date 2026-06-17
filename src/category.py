c28afe87bd5785e6c1e06bdd48a417bd1598029d
имя фамилия < >
2026-06-17 20:43:29 +0500

    14-2

--- src/category.py
+++ src/category.py
@@ -6,17 +6,34 @@ class Category:
 
     name: str
     description: str
-    products: list
+    __products: list
 
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.total_categories += 1
         self.total_products_update()
 
     def total_products_update(self):
-        Category.total_products += len(self.products)
+        Category.total_products += len(self.__products)
+
+    def add_product(self, product):
+        """Добавляет товар в категорию"""
+        if product not in self.__products:
+            self.__products.append(product)
+            Category.total_products += 1
+
+    @property
+    def products(self):
+        """Геттер для получения списка товаров в виде строки"""
+        if not self.__products:
+            return "В категории нет товаров"
+        result = []
+        for product in self.__products:
+            result.append(f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.")
+
+        return "\n".join(result)
 
     @classmethod
     def get_category_count(cls):
